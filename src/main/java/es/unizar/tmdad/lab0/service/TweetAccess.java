5bc9f7efd5965c051061fcf62c56367a7610f3ad
Carlos Sánchez <680321@unizar.es>
2018-03-28 19:47:28 +0200

    Get tweets from BD

---
+++ src/main/java/es/unizar/tmdad/lab0/service/TweetAccess.java
@@ -0,0 +1,44 @@
+package es.unizar.tmdad.lab0.service;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import org.springframework.stereotype.Service;
+
+import es.unizar.tmdad.lab0.repo.TweetRepository;
+import es.unizar.tmdad.lab0.repo.TweetSaved;
+
+
+@Service
+public class TweetAccess {
+	@Autowired
+	private TweetRepository repo;
+	
+	public Set<String> findQueries(){
+		Iterable<TweetSaved> it =  repo.findAll();
+		Set<String> resultSet = new LinkedHashSet<String>();
+		for(TweetSaved t: it){
+			resultSet.add(t.getQuery());
+			
+		}		
+		return resultSet;
+	}
+	
+	public ArrayList<TweetSaved> findByQuery(String q){
+		Iterable<TweetSaved> it = repo.findAll();
+		ArrayList<TweetSaved> resultSet = new ArrayList<TweetSaved>();
+		for(TweetSaved t: it){
+			String query = t.getQuery().replace(" ", "");
+			if(query.equals(q)){
+				resultSet.add(t);
+			}
+		}
+		
+		return resultSet;
+	}
+
+}
